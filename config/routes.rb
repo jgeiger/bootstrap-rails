170d7a69c76940606df5bf44ab02a3ba16a16531
jgeiger <jgeiger@gmail.com>
2013-01-19 22:40:59 -0600

    Update routes file, add rake rails:template version for post create

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,14 @@
 APP_NAME::Application.routes.draw do
 
-  match "/auth/:provider/callback", to: "sessions#create"
-  match "/auth/failure", to: "sessions#failure"
-  match "/sign_out", to: "sessions#destroy", as: "sign_out"
-  match "/sign_in" => redirect("/auth/google_oauth2"), as: "sign_in"
-  match "/dashboard", to: "users#dashboard", as: "dashboard"
-  match "/help", to: "pages#help", as: :help
+  get "/auth/:provider/callback", to: "sessions#create"
+  get "/auth/failure", to: "sessions#failure"
+  get "/sign_out", to: "sessions#destroy", as: "sign_out"
+  get "/sign_in" => redirect("/auth/google_oauth2"), as: "sign_in"
+  get "/dashboard", to: "users#dashboard", as: "dashboard"
+  get "/help", to: "pages#help", as: :help
 
   resources :sessions
   resources :users
 
-  root :to => 'pages#help'
+  root to: 'pages#help'
 end
